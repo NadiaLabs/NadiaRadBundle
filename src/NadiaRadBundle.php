8ae53a60f4765032fc9fc32626be6ff65860b8e8
Leo <leo.on.the.earth@gmail.com>
2023-05-22 00:01:33 +0800

    Move NadiaRadExtension into NadiaRadBundle

--- src/NadiaRadBundle.php
+++ src/NadiaRadBundle.php
@@ -14,10 +14,13 @@ namespace Nadia\Bundle\NadiaRadBundle;
 use Nadia\Bundle\NadiaRadBundle\DependencyInjection\Compiler\MenuBuilderPass;
 use Nadia\Bundle\NadiaRadBundle\DependencyInjection\Compiler\RoleTreeBuilderPass;
 use Nadia\Bundle\NadiaRadBundle\DependencyInjection\Compiler\UserRoleUpdaterPass;
-use Nadia\Bundle\NadiaRadBundle\DependencyInjection\NadiaRadExtension;
+use Nadia\Bundle\NadiaRadBundle\Menu\MenuProvider;
+use Nadia\Bundle\NadiaRadBundle\Security\Role\RoleHierarchyProvider;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class NadiaRadBundle extends AbstractBundle
@@ -29,8 +32,35 @@ class NadiaRadBundle extends AbstractBundle
         $container->addCompilerPass(new UserRoleUpdaterPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 10);
     }
 
-    public function getContainerExtension(): ?ExtensionInterface
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        return new NadiaRadExtension();
+        $loader = new PhpFileLoader($builder, new FileLocator(__DIR__ . '/../config'));
+
+        $this->loadMenuServices($loader, $builder);
+        $this->loadSecurityServices($loader, $builder);
+    }
+
+    private function loadMenuServices(PhpFileLoader $loader, ContainerBuilder $builder): void
+    {
+        $loader->load('menu.php');
+
+        $menuCacheDir = $builder->getParameter('nadia.menu.cache_dir');
+        if (!file_exists($menuCacheDir)) {
+            mkdir($menuCacheDir, 0755, true);
+        }
+
+        $builder->getDefinition(MenuProvider::class)->setArgument(1, $menuCacheDir);
+    }
+
+    private function loadSecurityServices(PhpFileLoader $loader, ContainerBuilder $builder): void
+    {
+        $loader->load('security.php');
+
+        $roleTreeCacheDir = $builder->getParameter('nadia.security.role_tree.cache_dir');
+        if (!file_exists($roleTreeCacheDir)) {
+            mkdir($roleTreeCacheDir, 0755, true);
+        }
+
+        $builder->getDefinition(RoleHierarchyProvider::class)->setArgument(1, $roleTreeCacheDir);
     }
 }
